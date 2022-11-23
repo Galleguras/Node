204f6192505c7cedc13f0f75659e9499f050c063
Pablo Gallego <pablo.gallego-carmona@t-systems.com>
2022-11-23 16:25:04 +0100

    segunda a`roximacion previa a mongo db

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
 let notes = [
   {
     id: 1,
@@ -19,11 +25,62 @@ let notes = [
     important: true,
   },
 ];
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(notes));
+
+const generateId = () => {
+  const notesIds = notes.map((n) => n.id);
+  const maxId = notesIds.length ? Math.max(...notesIds) : 0;
+  const newId = maxId + 1;
+  return newId;
+};
+
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server is running port ${PORT}`);
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    return response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+app.post("/api/notes", (request, response) => {
+  const note = request.body;
+
+  if (!note.content) {
+    return response.status(400).json({
+      error: 'required "content" field is missing',
+    });
+  }
+
+  const newNote = {
+    id: generateId(),
+    content: note.content,
+    date: new Date(),
+    import: note.important || false,
+  };
+
+  notes = notes.concat(newNote);
+
+  response.json(note);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
